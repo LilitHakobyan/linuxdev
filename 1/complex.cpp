7be037b11187210b114d923b753e777d5024d8cd
LilitHakobyan <lhakobyan1994@gmail.com>
2021-03-11 17:50:26 +0400

    created complex number class and makefile

---
+++ 1/complex.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+// complex class
+class Complex
+{
+    // declares complex number parts
+    private:
+      int real;
+      int imag;
+
+     public:
+       Complex(): real(0), imag(0){ }
+
+     public:
+       Complex(int r, int i){
+           this->real = r;
+           this->imag = i;
+       }
+
+
+       // + Operator overloading
+       Complex operator - (Complex c2)
+       {
+           Complex temp;
+           temp.real = this->real - c2.real;
+           temp.imag = this->imag - c2.imag;
+
+           return temp;
+       }
+
+       // - Operator overloading
+       Complex operator + (Complex c2)
+       {
+           Complex temp;
+           temp.real = this->real + c2.real;
+           temp.imag = this->imag + c2.imag;
+
+           return temp;
+       }
+
+       // multiply by constant 
+       Complex operator * (int a){
+            return  Complex(this->real * a, this->imag * a);
+       }
+
+       // calc abs
+       int absaluteValue(){
+           return sqrt(this->real* this->real + this->imag * this->imag);
+       }
+
+
+       // print complex number
+       void output()
+       {
+           if(imag < 0)
+               cout << "Output Complex number: "<< this->real << this->imag << "i \n";
+           else
+               cout << "Output Complex number: " << this->real << "+" << this->imag << "i \n";
+       }
+
+};
+
---
+++ 1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "complex.cpp"
+
+using namespace std;
+
+// sorting complex vector
+vector<Complex> ComplexVectorSort(std::vector<Complex> arr){
+    for(int i = 0; i < arr.size()-1; ++i){
+        if(arr[i].absaluteValue() > arr[i+1].absaluteValue()){
+            swap(arr[i],arr[i+1]);
+            // arr[i].output();
+        }
+    }
+
+    return arr;
+}
+
+// start pont for app
+int main()
+{
+    // // declare result
+    // Complex result;
+
+    // // init complex numbers
+    //  Complex c1 = Complex(9,5);
+    //  Complex c2 = Complex(5,9);
+
+    // // text - operator
+    // result = c1 - c2;
+    // result.output();
+
+   cout << "Init complex vector \n";
+    // create complex vector and init
+    vector<Complex> complexVecor(5);
+    for(int i = 0;i < complexVecor.size(); ++i){
+        int a ,b;
+        cin >> a;
+        cin >> b;
+        complexVecor[i] = Complex(a,b);
+        complexVecor[i].output();
+    }
+
+    //sort vector
+    vector<Complex> sortedVec = ComplexVectorSort(complexVecor);
+
+    cout << "print complex sorted vector \n";
+    // print sorted vector
+     for(int j = 0;j < sortedVec.size(); ++j){
+        sortedVec[j].output();
+    }
+
+    return 0;
+}
